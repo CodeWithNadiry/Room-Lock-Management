2b1fb81879650d8a0b93d1cd48ac2128adcd2713
nadiry <usmannadiry75@gmail.com>
2026-03-29 19:31:30 +0500

    changed auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,23 +4,22 @@ import { findUserByEmail } from "../services/auth.services.js";
 
 export async function login(req, res, next) {
   try {
-    
+    console.log("BODY:", req.body);
+
     const { email, password } = req.body;
 
     const user = await findUserByEmail(email);
+    console.log("USER:", user);
+
     if (!user) {
       const err = new Error("User not found");
       err.statusCode = 401;
       return next(err);
     }
 
-    if (!user.is_active) {
-      const err = new Error("User is inactive, contact admin!");
-      err.statusCode = 403;
-      return next(err);
-    }
-
     const isEqual = await bcrypt.compare(password, user.password);
+    console.log("PASSWORD MATCH:", isEqual);
+
     if (!isEqual) {
       const err = new Error("Incorrect password");
       err.statusCode = 401;
@@ -35,20 +34,13 @@ export async function login(req, res, next) {
         property_id: user.property_id || null,
       },
       process.env.JWT_SECRET,
-      { expiresIn: process.env.JWT_EXPIRES_IN },
+      { expiresIn: process.env.JWT_EXPIRES_IN }
     );
 
-    res.status(200).json({
-      token,
-      user: {
-        id: user.id,
-        name: user.name,
-        role: user.role,
-        email: user.email,
-        property_id: user.property_id,
-      },
-    });
+    res.status(200).json({ token, user });
+
   } catch (err) {
+    console.error("LOGIN ERROR:", err);
     err.statusCode ||= 500;
     next(err);
   }
